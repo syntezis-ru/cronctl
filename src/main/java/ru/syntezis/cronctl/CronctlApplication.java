36e1220d844014a0bf45086fe6cfe5db522ba847
silenzzz <silenzzzdev@gmail.com>
2026-04-30 20:38:48 +0300

    Initial commit

---
+++ src/main/java/ru/syntezis/cronctl/CronctlApplication.java
@@ -0,0 +1,13 @@
+package ru.syntezis.cronctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronctlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronctlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/syntezis/cronctl/CronctlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.syntezis.cronctl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronctlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
